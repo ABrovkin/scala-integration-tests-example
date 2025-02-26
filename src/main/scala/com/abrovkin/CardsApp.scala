c5473109ddefde5860ba5c85306c56089b626bf7
Anton Brovkin <a.a.brovkin@tinkoff.ru>
2025-02-26 19:48:44 +0300

    Warnings fixed

--- src/main/scala/com/abrovkin/CardsApp.scala
+++ src/main/scala/com/abrovkin/CardsApp.scala
@@ -3,8 +3,8 @@ package com.abrovkin
 import cats.effect.*
 import com.abrovkin.wirings.ProgramWiring.buildApp
 
-object App extends IOApp.Simple:
+object CardsApp extends IOApp.Simple:
 
-  val run = buildApp[IO]("http://localhost:8081", "redis://localhost:6379", "0.0.0.0", 8080).use { server =>
+  val run: IO[Unit] = buildApp[IO]("http://localhost:8081", "redis://localhost:6379", "0.0.0.0", 8080).use { server =>
     server.serve.compile.drain
   }
