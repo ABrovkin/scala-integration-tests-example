0860c691a45d4f9fcfdb47d1bb8ecc37b2d1d676
a.a.brovkin <a.a.brovkin@tbank.ru>
2025-10-21 18:44:21 +0300

    chore: removed unused imports

--- cards-app/src/test/scala/com/abrovkin/service/CardServiceSpec.scala
+++ cards-app/src/test/scala/com/abrovkin/service/CardServiceSpec.scala
@@ -1,6 +1,5 @@
 package com.abrovkin.service
 
-import cats.data.NonEmptyList
 import cats.effect.IO
 import cats.effect.testing.scalatest.AsyncIOSpec
 import com.abrovkin.model.UserId
@@ -13,9 +12,6 @@ import org.http4s.{EntityDecoder, Uri}
 import org.scalamock.scalatest.AsyncMockFactory
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
-
-import java.time.Instant
-import scala.concurrent.duration.{Duration, FiniteDuration}
 
 class CardServiceSpec extends AsyncFlatSpec with Matchers with AsyncMockFactory with AsyncIOSpec:
 
